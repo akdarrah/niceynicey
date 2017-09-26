85e71664c27709e4b21664aec4d05c6d98ca82d5
Adam Darrah <piremies@gmail.com>
2017-09-25 22:34:11 -0400

    Add Task State machine

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,16 @@
 class Task < ApplicationRecord
+  include AASM
+
   belongs_to :parent, class_name: "Task"
   belongs_to :user
+
+  aasm do
+    state :pending, :initial => true
+    state :completed
+
+    event :complete do
+      transitions :from => :pending, :to => :completed
+    end
+  end
+
 end
