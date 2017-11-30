69ebbd44ea94272bad5be9101221db2330492860
akdarrah <piremies@gmail.com>
2017-11-29 22:03:21 -0500

    Function to calculate number of completed tasks

--- app/assets/javascripts/components/todo_list/project_container.component.js
+++ app/assets/javascripts/components/todo_list/project_container.component.js
@@ -23,6 +23,7 @@
     vm.placeHolderText = placeHolderText;
     vm.createCheckpoint = createCheckpoint;
     vm.showForm = showForm;
+    vm.completedTaskCount = completedTaskCount;
 
     vm.tasks = [];
     vm.parentTask = null;
@@ -76,6 +77,24 @@
 
     function showForm(){
       return !vm.readOnly && (!vm.parentTask || vm.parentTask.state == 'pending')
+    }
+
+    function completedTaskCount(tasks){
+      var parentTasks = tasks || vm.tasks || [];
+
+      var completedCount = parentTasks.map(function(task){
+        return (task.state == "completed" ? 1 : 0);
+      }).reduce(function(sum, number){
+        return sum + number;
+      }, 0);
+
+      var completedChildrenCount = parentTasks.map(function(task){
+        return completedTaskCount(task.children);
+      }).reduce(function(sum, number){
+        return sum + number;
+      }, 0);
+
+      return completedCount + completedChildrenCount;
     }
 
   }
