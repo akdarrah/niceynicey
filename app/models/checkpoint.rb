8481430cb80c36b3b2963c7db8f87e72cafaaaf9
akdarrah <piremies@gmail.com>
2017-12-17 19:46:37 -0500

    Refactor logic for creating checkpoint

--- app/models/checkpoint.rb
+++ app/models/checkpoint.rb
@@ -4,35 +4,41 @@ class Checkpoint < ApplicationRecord
 
   validates :tasks, presence: true
 
-  def self.create_checkpoint!(user)
+  # parent_task = user.tasks
+  #   .no_checkpoint
+  #   .where(id: parent_task_id)
+  #   .first
+  #
+  # eligible_task_ids = if parent_task.present?
+  #   parent_task.lineage.map(&:id)
+  # else
+  #   user.tasks.pluck(:id)
+  # end
+
+
+  def self.create_checkpoint!(user, parent_task_id=nil)
+    qualifying_tasks = user.tasks
+      .top_level
+      .no_checkpoint
+      .select(&:completed_or_has_completed_child?)
+
     checkpoint = user.checkpoints.new
 
     transaction do
-      qualifying_tasks = user.tasks
-        .top_level
-        .no_checkpoint
-        .select(&:completed_or_has_completed_child?)
-
-      duplicated_tasks = qualifying_tasks.map do |task|
-        duplicate = task.dup_with_children do |duplicating_task|
+      qualifying_tasks.each do |qualifying_task|
+        duplicate = qualifying_task.dup_with_children do |duplicating_task|
           duplicating_task.completed_or_has_completed_child?
+        end
+
+        qualifying_task.traverse do |task|
+          task.archive! if task.completed?
         end
 
         duplicate.traverse do |task|
           task.checkpoint = checkpoint
         end
 
-        duplicate
-      end
-
-      qualifying_tasks.each do |qualifying_task|
-        qualifying_task.traverse do |task|
-          task.archive! if task.completed?
-        end
-      end
-
-      duplicated_tasks.each do |duplicated_task|
-        checkpoint.tasks << duplicated_task
+        checkpoint.tasks << duplicate
       end
 
       checkpoint.save!
@@ -40,4 +46,5 @@ class Checkpoint < ApplicationRecord
 
     checkpoint
   end
+  
 end
