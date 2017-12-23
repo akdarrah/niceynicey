55f0cc0e373f5e0f90c93c5989c1e2c782f5c8d2
akdarrah <piremies@gmail.com>
2017-12-23 02:07:47 -0500

    Analytics are viewable in the app

--- app/assets/javascripts/components/todo_list/project_container.component.js
+++ app/assets/javascripts/components/todo_list/project_container.component.js
@@ -27,6 +27,7 @@
     vm.completedTaskCount = completedTaskCount;
     vm.quote = gon.quote;
 
+    vm.analytics = {};
     vm.tasks = [];
     vm.parentTask = null;
     vm.tasksIndex = false;
@@ -68,41 +69,29 @@
         getTasks();
       }
 
-      // Javascript
-      var data = {
-        labels: ["12am-3am", "3am-6am", "6am-9am", "9am-12pm",
-          "12pm-3pm", "3pm-6pm", "6pm-9pm", "9pm-12am"],
+      if(vm.readOnly){
+        return;
+      }
 
-        datasets: [
-          {
-            title: "Some Data",
-            values: [25, 40, 30, 35, 8, 52, 17, -4]
-          },
-          {
-            title: "Another Set",
-            values: [25, 50, -10, 15, 18, 32, 27, 14]
-          },
-          {
-            title: "Yet Another",
-            values: [15, 20, -3, -15, 58, 12, -17, 37]
-          }
-        ]
-      };
+      var getAnalytics;
 
-      var chart = new Chart({
-        parent: "#chart", // or a DOM element
-        title: "My Awesome Chart",
-        data: data,
-        type: 'bar', // or 'line', 'scatter', 'pie', 'percentage'
-        height: 250,
+      if(vm.parentTask){
+        getAnalytics = vm.factory.getAnalytics(vm.parentTask.id);
+      } else {
+        getAnalytics = vm.factory.getAnalytics();
+      }
 
-        colors: ['#7cd6fd', 'violet', 'blue'],
-        // hex-codes or these preset colors;
-        // defaults (in order):
-        // ['light-blue', 'blue', 'violet', 'red',
-        // 'orange', 'yellow', 'green', 'light-green',
-        // 'purple', 'magenta', 'grey', 'dark-grey']
+      getAnalytics.then(function(response){
+        vm.analytics = response.data;
 
+        var heatmap = new Chart({
+          parent: "#heatmap",
+          type: 'heatmap',
+          height: 115,
+          data: vm.analytics,
+          discrete_domains: 1,
+          legend_colors: ['#ebedf0', '#c6e48b', '#7bc96f', '#239a3b', '#196127']
+        });
       });
     }
 
