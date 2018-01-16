a3a87731a9f23a051ab8e12d3830c3dc63c4eee3
akdarrah <piremies@gmail.com>
2018-01-15 22:26:44 -0500

    Deploy marketing site

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -1,7 +1,17 @@
 class MarketingController < ApplicationController
+  before_action :redirect_signed_in_users
+
   layout "marketing"
 
   def index
+  end
+
+  private
+
+  def redirect_signed_in_users
+    if current_user.present?
+      redirect_to tasks_url
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  root to: 'marketing#index'
 
   get '/checkpoints', to: redirect('/archives')
   get '/checkpoints/:id', to: redirect('/archives/%{id}')
